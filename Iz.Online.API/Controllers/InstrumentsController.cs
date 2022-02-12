a75d95688cdf644db38478a38e4d710a253b3ce2
alikeb <alikeb1990@gmail.com>
2022-02-12 12:45:06 +0330

    1

--- Iz.Online.API/Controllers/InstrumentsController.cs
+++ Iz.Online.API/Controllers/InstrumentsController.cs
@@ -13,7 +13,7 @@ namespace Iz.Online.API.Controllers
 {
     [Produces("application/json")]
     [Route("V1/[controller]")]
-    
+
 
     /// <summary>
     ///نام دیده بان
@@ -26,7 +26,6 @@ namespace Iz.Online.API.Controllers
         private readonly IExternalInstrumentService _externalInstrumentService;
         private readonly IDistributedCache _redis;
 
-
         public InstrumentsController(IInstrumentsService instrumentsService, IExternalInstrumentService externalInstrumentService, IDistributedCache redis)
         {
             _instrumentsService = instrumentsService;
@@ -37,15 +36,15 @@ namespace Iz.Online.API.Controllers
         #endregion
 
         [HttpPost("UpdateInstrumentsDb")]
-        
+
         public ResultModel<bool> UpdateInstrumentsDb([FromBody] ViewBaseModel model)
         {
             var updateResult = _externalInstrumentService.UpdateInstrumentList(model);
             return new ResultModel<bool>(updateResult, updateResult);
         }
 
-        [HttpPost("List")]  
-        
+        [HttpPost("List")]
+
         public ResultModel<List<InstrumentList>> List()
         {
             var instruments = _instrumentsService.InstrumentList();
@@ -53,26 +52,26 @@ namespace Iz.Online.API.Controllers
         }
 
         [HttpPost("WatchLists")]
-        
+
         public ResultModel<List<WatchList>> WatchLists([FromBody] ViewBaseModel model)
         {
-            var result = _instrumentsService.UserWatchLists(model );
+            var result = _instrumentsService.UserWatchLists(model);
             return new ResultModel<List<WatchList>>(result);
         }
 
         [HttpPost("WatchListsDetails")]
-        
+
         public ResultModel<WatchListDetails> WatchListsDetails([FromBody] SearchWatchList model)
         {
-            var result = _instrumentsService.WatchListDetails(model );
+            var result = _instrumentsService.WatchListDetails(model);
             return new ResultModel<WatchListDetails>(result);
         }
 
         [HttpPost("DeleteWatchList")]
-        
+
         public ResultModel<List<WatchList>> DeleteWatchList([FromBody] SearchWatchList model)
         {
-            var result = _instrumentsService.DeleteWatchList(model );
+            var result = _instrumentsService.DeleteWatchList(model);
             return new ResultModel<List<WatchList>>(result);
         }
 
@@ -80,50 +79,50 @@ namespace Iz.Online.API.Controllers
         public ResultModel<WatchListDetails> NewWatchList([FromBody] NewWatchList model)
         {
             var result = _instrumentsService.NewWatchList(model);
-            return new ResultModel<WatchListDetails>(result); 
+            return new ResultModel<WatchListDetails>(result);
         }
 
         [HttpPost("AddInstrumentToWatchList")]
-        
+
         public ResultModel<WatchListDetails> AddInstrumentToWatchList([FromBody] EditEathListItems model)
         {
-            var result = _instrumentsService.AddInstrumentToWatchList(model );
-            return new ResultModel<WatchListDetails>(result); 
-            
+            var result = _instrumentsService.AddInstrumentToWatchList(model);
+            return new ResultModel<WatchListDetails>(result);
+
         }
 
         [HttpPost("RemoveInstrumentFromWatchList")]
-        
+
         public ResultModel<WatchListDetails> RemoveInstrumentFromWatchList([FromBody] EditEathListItems model)
         {
-            var result = _instrumentsService.RemoveInstrumentFromWatchList(model );
+            var result = _instrumentsService.RemoveInstrumentFromWatchList(model);
             return new ResultModel<WatchListDetails>(result);
-            
+
         }
 
         [HttpPost("InstrumentWatchLists")]
-        
+
         public ResultModel<List<WatchList>> InstrumentWatchLists([FromBody] InstrumentWatchLists model)
         {
-            var result = _instrumentsService.InstrumentWatchLists(model );
+            var result = _instrumentsService.InstrumentWatchLists(model);
             return new ResultModel<List<WatchList>>(result);
         }
 
         [HttpPost("BestLimits")]
-        
-        public ResultModel< Izi.Online.ViewModels.Instruments.BestLimit.BestLimits> BestLimits([FromBody] SelectedInstrument model)
+
+        public ResultModel<Izi.Online.ViewModels.Instruments.BestLimit.BestLimits> BestLimits([FromBody] SelectedInstrument model)
         {
-            var result = _externalInstrumentService.BestLimits(model );
-            return new ResultModel<Izi.Online.ViewModels.Instruments.BestLimit.BestLimits>(result);
+            var result = _externalInstrumentService.BestLimits(model);
+            return result;
         }
 
 
         [HttpPost("Detail")]
-
         public ResultModel<InstrumentDetail> Detail([FromBody] SelectInstrumentDetails model)
         {
             var result = _instrumentsService.Detail(model);
-            return new ResultModel<InstrumentDetail>(result);
+            return result;
+
         }
 
     }
