1d2d0a0c0efe9ca333cc511600f23ec322c381a1
Amir <Amir@DESKTOP-RJA94O5>
2022-02-14 17:00:48 +0330

    asssss

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = new MyClass();
+
+            var ser = JsonConvert.SerializeObject(t);
+
+            Console.WriteLine(ser);
+
+            var des = JsonConvert.DeserializeObject<MyClass>(ser);
+
+            Console.WriteLine(des);
+
+            Console.ReadKey();
+        }
+    }
+
+    public class MyClass
+    {
+        public int? id { get; set; }
+
+        public string name { get; set; }
+    }
+}
